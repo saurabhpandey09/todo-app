8721465d4b49260ee086222d655dd227ac87e7df
saurabh pandey <saurabh09102@gmail.com>
2021-06-13 18:39:17 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const listofItems = () => {
+    setItems((newItems) => {
+      return [...newItems, inputList];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className="center-div">
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input
+          type="text"
+          placeholder="Add a Items"
+          onChange={(e) => setInputList(e.target.value)}
+        />
+        <button onClick={listofItems}>+</button>
+        <ol>
+          {/*<li>{inputList}</li> */}
+          {items.map((itemval, indx) => {
+            return (
+              <ol key="indx">
+                <li>{itemval}</li>
+              </ol>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
